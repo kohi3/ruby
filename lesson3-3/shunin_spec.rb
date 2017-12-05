b4c72887de561c4716d0fbe7bd214b383700d8e4
kohi <kohi3kohi@gmail.com>
2017-12-06 00:32:12 +0900

    テスト結果修正

--- lesson3-3/shunin_spec.rb
+++ lesson3-3/shunin_spec.rb
@@ -4,7 +4,7 @@ describe Shunin do
   let(:shunin){Shunin.new}
 
   example '主任の給料は2倍＋１' do
-    expect(shunin.calculate_salary(100)).to eq 20
+    expect(shunin.calculate_salary(100)).to eq 201
 
   end
 
